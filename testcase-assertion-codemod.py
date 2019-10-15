260539803c5863d4e78607502049796a8af53068
Hans-Wilhelm Warlo <5417271+hanswilw@users.noreply.github.com>
2019-10-15 12:58:25 +0200

    Black

--- testcase-assertion-codemod.py
+++ testcase-assertion-codemod.py
@@ -40,7 +40,7 @@ def handle_equal(node):
     args, kwarg_list, msg_with_comma = parse_args_and_msg(node, 2)
 
     if len(args) != 2:
-        print(f'Potentially malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Potentially malformed: {node}: {astunparse.unparse(node)}\n")
         return
 
     if args[0] in ["True", "False", "None"]:
@@ -55,7 +55,7 @@ def handle_not_equal(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 2)
 
     if len(args) != 2:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
 
     if args[0] in ["True", "False", "None"]:
@@ -70,7 +70,7 @@ def handle_contains(node):
     args, kwargs, msg_with_comma = parse_args_and_msg(node, 2)
     if not kwargs:
         if len(args) <= 2:
-            return f'assert {args[1]} in {args[0]}'
+            return f"assert {args[1]} in {args[0]}"
     kwarg = kwargs[0].split("=")
     return f'assert {args[1]} in {args[0]} and {args[0]}["{kwarg[0]}"] == {kwarg[1]}'
 
@@ -78,7 +78,7 @@ def handle_contains(node):
 def handle_true(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 1)
     if len(args) != 1:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} is True{msg_with_comma}"
 
@@ -86,7 +86,7 @@ def handle_true(node):
 def handle_false(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 1)
     if len(args) != 1:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} is False{msg_with_comma}"
 
@@ -94,7 +94,7 @@ def handle_false(node):
 def handle_in(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 2)
     if len(args) != 2:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} in {args[1]}{msg_with_comma}"
 
@@ -102,7 +102,7 @@ def handle_in(node):
 def handle_not_in(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 2)
     if len(args) != 2:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} not in {args[1]}{msg_with_comma}"
 
@@ -110,7 +110,7 @@ def handle_not_in(node):
 def handle_is(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 2)
     if len(args) != 2:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} is {args[1]}{msg_with_comma}"
 
@@ -118,7 +118,7 @@ def handle_is(node):
 def handle_is_none(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 1)
     if len(args) != 1:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} is None{msg_with_comma}"
 
@@ -126,7 +126,7 @@ def handle_is_none(node):
 def handle_is_not_none(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 1)
     if len(args) != 1:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert {args[0]} is not None{msg_with_comma}"
 
@@ -134,22 +134,22 @@ def handle_is_not_none(node):
 def handle_is_instance(node):
     args, _, msg_with_comma = parse_args_and_msg(node, 2)
     if len(args) != 2:
-        print(f'Malformed: {node}: {astunparse.unparse(node)}\n')
+        print(f"Malformed: {node}: {astunparse.unparse(node)}\n")
         return
     return f"assert isinstance({args[0]}, {args[1]}){msg_with_comma}"
 
 
 assert_mapping = {
-    'assertEqual': handle_equal,
-    'assertNotEqual': handle_not_equal,
-    'assertTrue': handle_true,
-    'assertFalse': handle_false,
-    'assertIn': handle_in,
-    'assertNotIn': handle_not_in,
-    'assertIs': handle_is,
-    'assertIsNone': handle_is_none,
-    'assertIsNotNone': handle_is_not_none,
-    'assertIsInstance': handle_is_instance,
+    "assertEqual": handle_equal,
+    "assertNotEqual": handle_not_equal,
+    "assertTrue": handle_true,
+    "assertFalse": handle_false,
+    "assertIn": handle_in,
+    "assertNotIn": handle_not_in,
+    "assertIs": handle_is,
+    "assertIsNone": handle_is_none,
+    "assertIsNotNone": handle_is_not_none,
+    "assertIsInstance": handle_is_instance,
 }
 
 
@@ -162,7 +162,7 @@ def convert(node):
 
 def assert_patches(list_of_lines):
     patches = []
-    test = ast.parse(''.join(list_of_lines))
+    test = ast.parse("".join(list_of_lines))
 
     line_deviation = 0
     threshold_line = 0
@@ -176,24 +176,39 @@ def assert_patches(list_of_lines):
                 if isinstance(func, ast.Attribute):
                     is_func = True
 
-
         if is_func:
             converted = convert(node)
             if converted:
-                assert_line = node.col_offset*' ' + converted + "\n"
+                assert_line = node.col_offset * " " + converted + "\n"
                 end_line = node.end_lineno
 
                 if node.lineno < threshold_line:
                     continue
 
-                patches.append(codemod.Patch(node.lineno - line_deviation - 1, end_line_number=end_line - line_deviation, new_lines=assert_line))
+                patches.append(
+                    codemod.Patch(
+                        node.lineno - line_deviation - 1,
+                        end_line_number=end_line - line_deviation,
+                        new_lines=assert_line,
+                    )
+                )
 
-                comment_line = COMMENT_REGEX.search(list_of_lines[min(end_line - 1, len(list_of_lines) - 1)])
-                line_deviation += (end_line - node.lineno)
+                comment_line = COMMENT_REGEX.search(
+                    list_of_lines[min(end_line - 1, len(list_of_lines) - 1)]
+                )
+                line_deviation += end_line - node.lineno
 
                 if comment_line:
-                    comment = comment_line.group(1) + comment_line.group(2).lstrip() + "\n"
-                    patches.append(codemod.Patch(end_line - line_deviation, end_line_number=end_line - line_deviation, new_lines=comment))
+                    comment = (
+                        comment_line.group(1) + comment_line.group(2).lstrip() + "\n"
+                    )
+                    patches.append(
+                        codemod.Patch(
+                            end_line - line_deviation,
+                            end_line_number=end_line - line_deviation,
+                            new_lines=comment,
+                        )
+                    )
                     line_deviation -= 1
 
                 threshold_line = end_line
@@ -201,11 +216,10 @@ def assert_patches(list_of_lines):
     return patches
 
 
-
 def is_py(filename):
-    #return filename == './tienda/audit_logs/tests/test_models.py'
-    #return filename == './tienda/campaigns/tests/test_views.py'
-    return filename.split('.')[-1] == 'py'
+    # return filename == './tienda/audit_logs/tests/test_models.py'
+    # return filename == './tienda/campaigns/tests/test_views.py'
+    return filename.split(".")[-1] == "py"
 
 
 codemod.Query(assert_patches, path_filter=is_py).run_interactive()
