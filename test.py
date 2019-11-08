23df2ea8a4daab121c7584e86ab5338fc95ca4c4
Hans-Wilhelm Warlo <5417271+hanswilw@users.noreply.github.com>
2019-11-08 10:32:21 +0100

    Add more to test and format

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
 class ExampleTest:
-    def test_something():
+    def testSomething(self):
         self.assertEqual(1, 1)
         self.assertEqual(1, 1, msg="1 should always be 1")
         self.assertEqual(1, 1, "1 should always be 1")
+        self.assertEqual(1, 1) # 1 should always be one
 
-        def inner_test_method():
+        def innerTestMethod():
             self.assertEqual(1, 1)
+            with self.assertRaises(ValueError):  # This error is always raised!
+                raise ValueError("SomeError")
 
-        inner_test_method()
+        innerTestMethod()
 
         self.assertEqual(1, 1)
+
+    def test_lots_of_arguments(self):
+        def inside_another_function():
+
+            self.assertEqual(
+                get_product_from_backend_product_with_supplier_product_and_cart_etc_etc_etc(
+                    product__backend_product_id=self.backend_product.id,
+                    product_id=self.product.id,
+                    name="Julebrus",
+                ),  # Some comment here
+                True,
+            )
+            self.assertTrue(True)
+
+        self.assertFalse(False)
+
