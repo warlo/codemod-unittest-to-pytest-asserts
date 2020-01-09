6e9628e01f97d92dfafcc27276cd4ae6cf87c64b
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2020-01-09 10:11:14 +0100

    Use pathlib

--- codemod_unittest_to_pytest_asserts/tests/test_codemod.py
+++ codemod_unittest_to_pytest_asserts/tests/test_codemod.py
@@ -1,32 +1,38 @@
 import codemod
 import shutil
 import os
+import pathlib
 
 from ..codemod_unittest_to_pytest_asserts import *
 
 UNITTEST_TEMPLATE = "./unittest_code_template.py"
 UNITTEST_FILE = "unittest_code.py"
 PYTEST_FILE = "./pytest_code.py"
-DIRNAME = os.path.dirname(__file__)
+DIRNAME = pathlib.Path(__file__).parent
 
 
 def test():
 
-    unittest_code = open(os.path.join(DIRNAME, UNITTEST_FILE)).read()
+    unittest_code = (DIRNAME / UNITTEST_FILE).read_text()
 
     codemod.Query(
         assert_patches,
         path_filter=lambda x: x
-        == "./" + os.path.join(os.path.relpath(DIRNAME), UNITTEST_FILE),
+        == "./" + str((pathlib.PurePath(DIRNAME).relative_to(pathlib.Path.cwd()) / UNITTEST_FILE)),
     ).run_interactive()
 
     assert (
-        open(os.path.join(DIRNAME, UNITTEST_FILE)).read()
-        == open(os.path.join(DIRNAME, PYTEST_FILE)).read()
+        (DIRNAME / UNITTEST_FILE).read_text()
+        == (DIRNAME / PYTEST_FILE).read_text()
     )
+    assert (
+        (DIRNAME / UNITTEST_FILE).read_text()
+        != (DIRNAME / UNITTEST_TEMPLATE).read_text()
+    )
+    print("Tests passed!")
 
     # Overwrite the unittest file from the template again to avoid git
-    open(os.path.join(DIRNAME, UNITTEST_FILE), "w").write(unittest_code)
+    open((DIRNAME / UNITTEST_FILE), "w").write(unittest_code)
 
 
 if __name__ == "__main__":
